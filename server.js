efae4baa54b41c7766874c5b9646111163295414
gunjantd <gunjan.maheshwari@td.com>
2014-12-18 00:42:58 -0500

    chaged file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('p Welcome to #{title} with WebMatrix on Azure! Hello World\n');
+  res.end('p Welcome to #{title} with WebMatrix on Azure! Hello World\n FROM WTS Dev Services');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
